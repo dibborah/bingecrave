c9ecf7badcef31285564d17ecf9cc0ff4ad4b38e
dibya.borah <dibyajyotiborah469@gmail.com>
2025-04-29 01:03:22 +0530

    initializing build deployment

--- script.ts
+++ script.ts
@@ -1,3 +1,11 @@
 // npm install -D tailwindcss postcss autoprefixer
 
 // npx tailwindcss init -p (old tailwind version needed)
+
+// Steps to deploy project in github: 
+
+// make base url in vite config file
+// give the homepage property with url value in the package.json
+// Write the two scripts of deployment (refer: pedroTech)
+// install gh-pages
+// then commit
